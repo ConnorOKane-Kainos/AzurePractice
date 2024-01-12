8bfdf9e8e9be2ec07c159e2873001e4fdba54cdd
Connor <connor.o'kane@kainos.com>
2024-01-12 16:31:14 +0000

    updated dns config and fixed the ip address issue

--- boostrap/resource_group.tf
+++ boostrap/resource_group.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rsi_rg" {
-  name = "rsi_rg"
+  name     = "rsi_rg"
   location = "uksouth"
 }
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,10 +1,10 @@
 output "subnet1_id" {
-  value = azurerm_virtual_network.vnet1.subnet.*.id[0]
+  value       = azurerm_virtual_network.vnet1.subnet.*.id[0]
   description = "The ID of subnet2"
 }
 
 
 output "subnet2_id" {
-  value = azurerm_virtual_network.vnet2.subnet.*.id[0]
+  value       = azurerm_virtual_network.vnet2.subnet.*.id[0]
   description = "The ID of subnet2"
 }
