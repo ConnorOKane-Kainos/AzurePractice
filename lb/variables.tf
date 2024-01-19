94268952160402c23a874e03a0e99f3cfaa1a1ac
Connor <connor.o'kane@kainos.com>
2024-01-19 15:01:33 +0000

    updated load balancer config and vars

--- lb/variables.tf
+++ lb/variables.tf
@@ -12,9 +12,3 @@ variable "my_NIC" {
   type    = string
   default = "my_NIC"
 }
-
-# variable "network_interface_ids" {
-#   description = "List of network interface IDs to associate with the NAT rule"
-#   type        = list(string)
-#   default     = ["azurerm_network_interface.nic1.id", "azurerm_network_interface.nic2.id"] # Replace with actual Network Interface IDs
-# }
